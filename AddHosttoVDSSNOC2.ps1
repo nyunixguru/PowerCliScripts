78b18011cdcff84edbed07c7149c1a374db2a01f
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 15:55:20 -0500

    Update AddHosttoVDSSNOC2.ps1

--- AddHosttoVDSSNOC2.ps1
+++ AddHosttoVDSSNOC2.ps1
@@ -2,9 +2,9 @@
 #for ($i=1; $i -lt 5; $i++) 
 #{
 # Add Esxi host first to the VDS
-#Add-VDSwitchVMHost -vmhost snoc2-c5-b$i.srv.hcvlny.cv.net -VDSwitch "SNOC2_Vmotion_dvSwitch"
+#Add-VDSwitchVMHost -vmhost snoc2-c5-b$i.srv.hcvlnyet -VDSwitch "SNOC2_Vmotion_dvSwitch"
 #Start-Sleep -s 2
- #$NetworkAdapter = Get-VMHost -Name snoc2-c5-b$i.srv.hcvlny.cv.net | Get-VMHostNetworkAdapter `
+ #$NetworkAdapter = Get-VMHost -Name snoc2-c5-b$i.srv.hcvlnyt | Get-VMHostNetworkAdapter `
 # -Name vmnic1 -Physical
 # Start-Sleep -s 2
 #Add Physical network adapter uplink to VDS
@@ -15,16 +15,16 @@
 
 
 #Add Vmotion IP
-#New-VMHostNetworkAdapter -VMHost "snoc2-c5-b1.srv.hcvlny.cv.net" -PortGroup "SNOC2_Vmotion_dvPortGroup" -VirtualSwitch "SNOC2_Vmotion_dvSwitch" -IP 10.248.198.164 -SubnetMask 255.255.255.128 -VMotionEnabled:$true
+#New-VMHostNetworkAdapter -VMHost "snoc2-c5-b1.srv.hcvet" -PortGroup "SNOC2_Vmotion_dvPortGroup" -VirtualSwitch "SNOC2_Vmotion_dvSwitch" -IP 10.248.198.164 -SubnetMask 255.255.255.128 -VMotionEnabled:$true
 
 
 #SNOC2_Accsw101-102.NOCSF_dvSwitch
 #for ($i=1; $i -lt 5; $i++) 
 #{
 # Add Esxi host first to the VDS
-#Add-VDSwitchVMHost -vmhost snoc2-c5-b$i.srv.hcvlny.cv.net -VDSwitch "SNOC2_Vmotion_dvSwitch"
+#Add-VDSwitchVMHost -vmhost snoc2-c5-b$i.srv.hcvlet -VDSwitch "SNOC2_Vmotion_dvSwitch"
 #Start-Sleep -s 2
-# $NetworkAdapter = Get-VMHost -Name snoc2-c5-b$i.srv.hcvlny.cv.net | Get-VMHostNetworkAdapter `
+# $NetworkAdapter = Get-VMHost -Name snoc2-c5-b$i.srv.hcvlet | Get-VMHostNetworkAdapter `
  #-Name vmnic8 -Physical
  #Start-Sleep -s 2
 #Add Physical network adapter uplink to VDS
@@ -37,9 +37,9 @@
 for ($i=1; $i -lt 5; $i++) 
 {
 # Add Esxi host first to the VDS
-#Add-VDSwitchVMHost -vmhost snoc2-c5-b$i.srv.hcvlny.cv.net -VDSwitch "SNOC2_DWSW1-2.NOCSF_dvSwitch"
+#Add-VDSwitchVMHost -vmhost snoc2-c5-b$i.srv.hcvet -VDSwitch "SNOC2_DWSW1-2.NOCSF_dvSwitch"
 Start-Sleep -s 2
- $NetworkAdapter = Get-VMHost -Name snoc2-c5-b$i.srv.hcvlny.cv.net | Get-VMHostNetworkAdapter `
+ $NetworkAdapter = Get-VMHost -Name snoc2-c5-b$i.srv.hcet | Get-VMHostNetworkAdapter `
  -Name vmnic9 -Physical
  Start-Sleep -s 2
 #Add Physical network adapter uplink to VDS
