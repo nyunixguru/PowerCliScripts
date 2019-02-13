a95cc9beb633224a3c563cfca0e17fd1b5324a14
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:39:27 -0500

    Update setVM.ps1

--- setVM.ps1
+++ setVM.ps1
@@ -3,5 +3,5 @@
 Connect-VIServer -Server cld3-c3-b8.srv.hcvlny.cv.net -User root -Password r00t123
 
 
-Get-VM -Name ipmgmt4.hesv.hcvlny.dhg.cv.net | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName CLD3-VLAN105_dvPortGroup
-Get-VM -Name ipmgmt4.hesv.hcvlny.dhg.cv.net|Get-NetworkAdapter |Where {$_.NetworkName -eq "VM Network" } |Set-NetworkAdapter -NetworkName "CLD3-VLAN105_dvPortGroup" -Confirm:$false
+Get-VM -Name ipmgm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName CLD3-VLAN105_dvPortGroup
+Get-VM -Name ipmgmt|Get-NetworkAdapter |Where {$_.NetworkName -eq "VM Network" } |Set-NetworkAdapter -NetworkName "CLD3-VLAN105_dvPortGroup" -Confirm:$false
