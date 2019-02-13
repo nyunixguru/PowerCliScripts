fc7f89febae3ef9f4b1bec78d023587c5456b501
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:38:01 -0500

    Update removeVM.ps1

--- removeVM.ps1
+++ removeVM.ps1
@@ -1,7 +1,7 @@
 ﻿
 
-Connect-VIServer -Server cld3-c5-b8.srv.hcvlny.cv.net  -User root -Password r00t123
+Connect-VIServer -Server cld3-  -User root -Password r0
 
-Stop-VM -VM ipmgmt4.hesv.hcvlny.dhg.cv.net
-Remove-VM -VM   ipmgmt4.hesv.hcvlny.dhg.cv.net -DeletePermanently
+Stop-VM -VM ipmgmt
+Remove-VM -VM   ipmgmt -DeletePermanently
 #Remove-Inventory -Item  WebServerTest -Server 192.168.1.3
