0ef14dccfd9353656ae721305d66e3f00d6991da
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:13:33 -0500

    Update changeNetworkAdapter.ps1

--- changeNetworkAdapter.ps1
+++ changeNetworkAdapter.ps1
@@ -1,9 +1,9 @@
 ﻿
-Connect-VIServer -Server cld3-c3-b1.srv.hcvlny.cv.net  -User root -Password r00t123
-#Get-NetworkAdapter -vm vdhcpred1.srv.hcvlny.cv.net | remove-networkadapter -confirm:$False
-#New-NetworkAdapter -VM  vdhcpred1.srv.hcvlny.cv.net -Type vmxnet3 
+Connect-VIServer -Server cld -User root -Password r
+#Get-NetworkAdapter -vm vdh | remove-networkadapter -confirm:$False
+#New-NetworkAdapter -VM  vdh -Type vmxnet3 
 #-NetworkName CLD3-SWR3-4.SF_VLAN114_dvPortGroup -StartConnected | Out-Null
-#Get-VMHostNetwork -VMHost cld3-c3-b1.srv.hcvlny.cv.net 
+#Get-VMHostNetwork -VMHost cld3 
 
 #New-NetworkAdapter -VM $vm -NetworkName "VM Network" -MacAddress  'aa:bb:cc:dd:ee:ff' -WakeOnLan -StartConnected
 Get-VMHostFirewallException
