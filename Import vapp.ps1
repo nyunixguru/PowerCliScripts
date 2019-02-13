99adeb1083f33a102893f5a30168f9eb4f898326
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:05:00 -0500

    Update Import vapp.ps1

--- Import vapp.ps1
+++ Import vapp.ps1
@@ -1,7 +1,7 @@
 ﻿
 Connect-VIServer cld1-c4-User root -Password r
-Import-vApp -VMhost  cld1-c4- -Source "c:\vdhcphsd.srv.whplny.cv.net\vdhcp.net.ovf" -Name vdhcphsd14et `
--Datastore CLD1-HCVLNY-OS13
+Import-vApp -VMhost  cld1-c4- -Source "c:\vdhcphsdt\vdhcp.net.ovf" -Name vdhcphsd14et `
+-Datastore CLD1OS13
 #Connect-VIServer emd2 -User root -Password 
 #Import-vApp -VMhost  cld1-c1-b4net -Source  "d:\ISO_IMAGES\vdhcptemnet\vdhcptemplt.ovf" -Name  vemd2-oolstg004net `
 #-Datastore EMD2-OS17
