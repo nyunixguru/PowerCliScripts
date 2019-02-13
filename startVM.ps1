7202506a8a27ab4b4bf4c9c89f4c2afc20d3550b
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:40:49 -0500

    Delete startVM.ps1

--- startVM.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-
-Connect-VIServer -Server cld3-c3-b8.srv.hcvlny.cv.net -User root -Password r00t123
-
-#Stop-VM -VM ipmgmt3.hesv.hcvlny.dhg.cv.net
-Set-VM -VM ipmgmt3.hesv.hcvlny.dhg.cv.net  -MemoryGB 20
