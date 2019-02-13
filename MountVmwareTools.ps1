731d5011b71ce3500f3aefc14ba3d43431dda58a
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:05:46 -0500

    Update MountVmwareTools.ps1

--- MountVmwareTools.ps1
+++ MountVmwareTools.ps1
@@ -1,11 +1,11 @@
-﻿Connect-VIServer -Server cld3-c5-b8.srv.hcvlny.cv.net -User root -Password r00t123
+﻿Connect-VIServer -Server cld3- -User root -Password r
 
 
 
 #Unmount cd iso
 #Get-VM | Get-CDDrive | Where { $_.IsoPath } | Set-CDDrive -NoMedia -Confirm:$true
 
-#   Get-CDDrive -VM ipmgmt4.hesv.hcvlny.dhg.cv.net| 
+#   Get-CDDrive -VM ipmgmt4.hesv.net| 
   #   Set-CDDrive -IsoPath "[CLD3-RFDN-DS2]/rhel-server-6.4-i386-dvd.iso" `
  #        -StartConnected:$true `
 #        -Confirm:$true
