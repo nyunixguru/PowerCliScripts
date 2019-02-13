0d7f4eda5f75cae2ce0e6cbe4ec0e5ac08ea1c88
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:12:16 -0500

    Update UpdateVmwareTools.ps1

--- UpdateVmwareTools.ps1
+++ UpdateVmwareTools.ps1
@@ -1,2 +1,2 @@
-﻿Connect-VIServer -Server cld3-c4-b8.srv.hcvlny.cv.net -User root -Password r00t123
-Update-Tools rfdn.srv.hcvlny.cv.net
+﻿Connect-VIServer -Server cld3- -User root -Password 
+Update-Tools hostname
