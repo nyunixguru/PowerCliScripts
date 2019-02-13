7716f642e58d6b7ed4356a75341af9eef795a324
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:32:35 -0500

    Add files via upload

--- exportVapp1.ps1
+++ exportVapp1.ps1
@@ -1,2 +1,2 @@
-﻿Connect-VIServer cld1-c1-b5.srv.whplny.cv.net -User root -Password r00t123
-Export-VApp -Destination "c:\" -VM vdhcphsd15.srv.whplny.cv.net -Name vdhcphsd.srv.whplny.cv.net -RunAsync
+﻿Connect-VIServer cld1-c1-b5.srv.whplny. -User root -Password r00t123
+Export-VApp -Destination "c:\" -VM vdhcphsd15.srv.whplny. -Name vdhcphsd.srv.whplny. -RunAsync
