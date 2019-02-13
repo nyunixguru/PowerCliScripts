0200a5314247273e4d6a008dfdb6183dee9e61d6
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:38:23 -0500

    Update rescan_datastore.ps1

--- rescan_datastore.ps1
+++ rescan_datastore.ps1
@@ -1,3 +1,3 @@
 ﻿
-Connect-VIServer -Server cld1-c3-b1.srv.hcvlny.cv.net -User root -Password r00t123
+Connect-VIServer -Server cld1-c3 -User root -Password r00
 Get-VMHostStorage -RescanAllHba -RescanVmfs
