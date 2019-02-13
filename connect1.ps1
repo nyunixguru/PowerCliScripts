b4e5a77fa6dfbde9c24d98a5f7f9408f93976ff9
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:28:35 -0500

    Delete connect1.ps1

--- connect1.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-
-#Connect-VIServer -Server cld1-c1-b1.srv.whplny.cv.net -User root -Password vmware
-#Connect-VISrver -Server cld1-c1-b1.srv.whplny.cv.net -User root -Password r00t123
-#Connect-VIServer -Server cld2-vbprod-c1-b1.vsf5-2.hcvlny.dhg.cv.net -User root -Password V1rtu@1c3!
-Connect-VIServer -Server vbvc1-prod.vsf5-2.hcvlny.dhg.cv.net -User Administrator -Password V1rtu@1c3!
-Get-VM
-
-
