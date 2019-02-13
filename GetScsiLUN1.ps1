0cd549560a05258d1fe360dfc62e53652e669b18
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:01:38 -0500

    Update GetScsiLUN1.ps1

--- GetScsiLUN1.ps1
+++ GetScsiLUN1.ps1
@@ -1,4 +1,4 @@
-﻿Connect-VIServer -Server cld1-c3-b1.srv.hcvlny.cv.net -User root -Password r00t123
+﻿Connect-VIServer -Server cld1-c3  -User root -Password r00t123
 
 Get-VMHostStorage -RescanAllHba -RescanVmfs
 Get-VMHostStorage
