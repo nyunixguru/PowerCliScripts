d6da35da5a71585fd018c60846e66d18eb6dbaf6
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:37:27 -0500

    Update removeNetworkAdapter.ps1

--- removeNetworkAdapter.ps1
+++ removeNetworkAdapter.ps1
@@ -1,8 +1,8 @@
 ﻿
 
-Connect-VIServer -Server cld3-c3-b8.srv.hcvlny.cv.net  -User root -Password r00t123
-#Start-VM -vm ipmgmt3.hesv.hcvlny.dhg.cv.net  
-Stop-VM -vm ipmgmt3.hesv.hcvlny.dhg.cv.net  
-#Get-NetworkAdapter -vm ipmgmt3.hesv.hcvlny.dhg.cv.net | remove-networkadapter -confirm:$False
-#New-NetworkAdapter  -VM ipmgmt4.hesv.hcvlny.dhg.cv.net -NetworkName "CLD3-VLAN105_dvPortGroup"  -StartConnected -Type Vmxnet3
-#New-NetworkAdapter  -VM ipmgmt3.hesv.hcvlny.dhg.cv.net  -NetworkName "VM Network" -StartConnected -Type flexible
+Connect-VIServer -Server cld3-  -User root -Password r0
+#Start-VM -vm ipmgmt3.  
+Stop-VM -vm ipmgmt3. 
+#Get-NetworkAdapter -vm ipmgmt | remove-networkadapter -confirm:$False
+#New-NetworkAdapter  -VM ipmgmtt -NetworkName "CLD3-VLAN105_dvPortGroup"  -StartConnected -Type Vmxnet3
+#New-NetworkAdapter  -VM ipmgmt  -NetworkName "VM Network" -StartConnected -Type flexible
