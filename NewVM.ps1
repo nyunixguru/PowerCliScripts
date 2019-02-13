f6113a32ccd58be034019875187554dd0dd36bfa
nyunixguru <nyunixguru@yahoo.com>
2019-02-13 16:06:38 -0500

    Update NewVM.ps1

--- NewVM.ps1
+++ NewVM.ps1
@@ -1,7 +1,7 @@
 ﻿
-Connect-VIServer -Server cld3-c5-b8.srv.hcvlny.cv.net -User root -Password r00t123
+Connect-VIServer -Server cld3-c5 -User root -Password r0
 
-New-VM -Name rfdndata1.srv.hcvlny.cv.net `
+New-VM -Name rfdndat
    -Datastore CLD3-RFDN-DS1 `
    -DiskGB 100 `
    -DiskStorageFormat thick `
@@ -9,7 +9,7 @@ New-VM -Name rfdndata1.srv.hcvlny.cv.net `
    -NumCpu 2 `
    -GuestId rhel6_64Guest `
    -CD |
-   Get-CDDrive -VM rfdndata1.srv.hcvlny.cv.net| 
+   Get-CDDrive -VM rfdndata1t| 
       Set-CDDrive -IsoPath "[CLD3-RFDN-DS2]/rhel-server-6.4-x86_64-dvd.iso" `
          -StartConnected:$true `
          -Confirm:$False
